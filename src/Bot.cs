c484e635346d148ef065cee6f4f37a2e25c05d60
Azok42 <anton1.hackner@gmail.com>
2026-06-23 12:48:21 +0200

    feat: add bot setup logic and test message

--- src/Bot.cs
+++ src/Bot.cs
@@ -1,3 +1,8 @@
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace IndYBot;
 
 class Bot
@@ -6,6 +11,32 @@ class Bot
 
    public async Task MainAsync()
    {
+      var socketConfig = new DiscordSocketConfig
+      {
+         GatewayIntents = GatewayIntents.AllUnprivileged
+      };
 
+      using var services = new ServiceCollection()
+         .AddSingleton(socketConfig)
+         .AddSingleton<DiscordSocketClient>()
+         .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+         .BuildServiceProvider();
+
+      var client = services.GetRequiredService<DiscordSocketClient>();
+      var interactionService = services.GetRequiredService<InteractionService>();
+
+      string token = File.ReadAllText("bot-info/token").Trim();
+      await client.LoginAsync(TokenType.Bot, token);
+      await client.StartAsync();
+
+      await Task.Delay(9999);
+
+      var channel = client.GetChannel(1493851566854115400) as IMessageChannel;
+      if (channel != null)
+      {
+         await channel.SendMessageAsync("Test");
+      }
+
+      await Task.Delay(-1);
    }
 }
