455607517f3d9eb4f3a6e04ab288d993dfb9a2ec
Azok42 <anton1.hackner@gmail.com>
2026-06-25 09:18:39 +0200

    feat: add helper function for long list messages

---
+++ src/Helpers/MessageHelper.cs
@@ -0,0 +1,31 @@
+using Discord.Interactions;
+
+namespace IndYBot.Helpers;
+
+public class MessageHelper
+{
+   public static async Task SendListMessageAsync<T>(List<T> list, SocketInteractionContext context, Func<T, string> msgListItem, string? firstMsg = null)
+   {
+      bool sendExtraMsg = false;
+      string msg = firstMsg ?? "";
+      foreach (var element in list)
+      {
+         string tmpMsg = msgListItem(element);
+
+         if (msg.Count() + tmpMsg.Count() >= 2000)
+         {
+            await context.Channel.SendMessageAsync(msg);
+            msg = tmpMsg;
+            sendExtraMsg = false;
+         }
+         else
+         {
+            msg += tmpMsg;
+            sendExtraMsg = true;
+         }
+      }
+
+      if (sendExtraMsg)
+         await context.Channel.SendMessageAsync(msg);
+   }
+}
