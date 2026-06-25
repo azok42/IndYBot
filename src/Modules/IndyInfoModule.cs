e814347e7202dcef840d5138611aff21357cab19
Azok42 <anton1.hackner@gmail.com>
2026-06-25 08:33:28 +0200

    feat: add special indy command

--- src/Modules/IndyInfoModule.cs
+++ src/Modules/IndyInfoModule.cs
@@ -26,4 +26,34 @@ public class IndyInfoModule : InteractionModuleBase<SocketInteractionContext>
 
       await Context.Channel.SendMessageAsync(msg);
    }
+
+   [SlashCommand("specialindy", "Get a list of current special indys!")]
+   public async Task SpecialIndyCommand()
+   {
+      var specialIndys = await IndyClient.GetSpecialIndyAsync();
+
+      await RespondAsync("Getting all special indys...", ephemeral: true);
+
+      bool sendExtraMsg = false;
+      string msg = "# Special Indy:\n";
+      foreach (var specialIndy in specialIndys)
+      {
+         string tmpMsg = $"- {specialIndy}\n";
+
+         if (msg.Count() + tmpMsg.Count() >= 2000)
+         {
+            await Context.Channel.SendMessageAsync(msg);
+            msg = tmpMsg;
+            sendExtraMsg = false;
+         }
+         else
+         {
+            msg += tmpMsg;
+            sendExtraMsg = true;
+         }
+      }
+
+      if (sendExtraMsg)
+         await Context.Channel.SendMessageAsync(msg);
+   }
 }
