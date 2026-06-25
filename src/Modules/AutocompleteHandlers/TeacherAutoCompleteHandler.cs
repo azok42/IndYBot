805f8f564ed6b5ecb96d8497646ac30b65c5ebdd
Azok42 <anton1.hackner@gmail.com>
2026-06-25 10:36:45 +0200

    feat: add autocompletion to search for teachers in special indy

---
+++ src/Modules/AutocompleteHandlers/TeacherAutoCompleteHandler.cs
@@ -0,0 +1,35 @@
+using Discord;
+using Discord.Interactions;
+using IndYLib.Services;
+
+namespace IndYBot.Modules.AutocompleteHandlers;
+
+public class TeacherAutoCompleteHandler : AutocompleteHandler
+{
+   public override async Task<AutocompletionResult> GenerateSuggestionsAsync(
+         IInteractionContext context,
+         IAutocompleteInteraction autocompleteInteraction,
+         IParameterInfo parameter,
+         IServiceProvider services)
+   {
+      string input = autocompleteInteraction.Data.Current.Value?.ToString() ?? "";
+
+      try
+      {
+          var specialIndys = await IndyClient.GetSpecialIndyAsync();
+          List<string> teacherIDs = specialIndys.Select(teachers => teachers.TeacherId).ToList();
+
+          var suggestion = teacherIDs
+             .Where(x => x.Contains(input, StringComparison.OrdinalIgnoreCase))
+             .Distinct()
+             .Select(x => new AutocompleteResult(x, x.ToLower()))
+             .Take(25);
+
+          return AutocompletionResult.FromSuccess(suggestion);
+      }
+      catch (Exception e)
+      {
+         return AutocompletionResult.FromError(e);
+      }
+   }
+}
--- src/Modules/IndyInfoModule.cs
+++ src/Modules/IndyInfoModule.cs
@@ -1,6 +1,7 @@
 using Discord.Interactions;
 using IndYLib.Services;
 using IndYBot.Helpers;
+using IndYBot.Modules.AutocompleteHandlers;
 
 namespace IndYBot.Modules;
 
@@ -22,9 +23,16 @@ public class IndyInfoModule : InteractionModuleBase<SocketInteractionContext>
    }
 
    [SlashCommand("specialindy", "Get a list of current special indys!")]
-   public async Task SpecialIndyCommand()
+   public async Task SpecialIndyCommand(
+         [Summary("teacher-id", "Only show special indys from teacher")]
+         [Autocomplete(typeof(TeacherAutoCompleteHandler))] string teacherId = "")
    {
       var specialIndys = await IndyClient.GetSpecialIndyAsync();
+
+      if (!string.IsNullOrWhiteSpace(teacherId))
+         specialIndys = specialIndys.Where(x => x.TeacherId.Contains(teacherId, StringComparison.OrdinalIgnoreCase)).ToList();
+
+      Console.WriteLine(specialIndys.Count);
 
       await RespondAsync("Getting all special indys...", ephemeral: true);
 
