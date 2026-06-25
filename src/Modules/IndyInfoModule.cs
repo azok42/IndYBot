acd7262f625ef975cbb535ef06f5f1b99f90f4b0
Azok42 <anton1.hackner@gmail.com>
2026-06-25 11:53:17 +0200

    feat: add get indy hour command

--- src/Modules/IndyInfoModule.cs
+++ src/Modules/IndyInfoModule.cs
@@ -44,4 +44,62 @@ public class IndyInfoModule : InteractionModuleBase<SocketInteractionContext>
             element => $"- {element.TeacherId} \t {element.AreaOfExpertise} on {element.Day} {element.Hour} ({element.StartDate} - {element.EndDate})\n",
             firstMsg);
    }
+
+   public enum Day
+   {
+      [ChoiceDisplay("Monday")]
+      Mo,
+
+      [ChoiceDisplay("Wendsday")]
+      Mi,
+
+      [ChoiceDisplay("Friday")]
+      Fr
+   }
+
+   public enum Hour
+   {
+      [ChoiceDisplay("3")]
+      Hour3 = 3,
+
+      [ChoiceDisplay("4")]
+      Hour4 = 4
+   }
+
+   [SlashCommand("hours", "get a list of all indy hours")]
+   public async Task IndyHourCommand(
+         [Summary("hour", "Show only hours in hour")] Hour? hour = null,
+         [Summary("day", "Show only hours on day")] Day? day = null,
+         [Summary("teacher", "Only show hours from teacher")]
+         [Autocomplete(typeof(TeacherAutoCompleteHandler))] string teacherId = "")
+   {
+      await RespondAsync("Getting all indy hours...", ephemeral: true);
+
+      var indyHours = await IndyClient.GetIndyHoursAsync();
+
+      if (hour != null)
+         indyHours = indyHours.Where(x => (Hour) x.Hour == hour.Value).ToList();
+
+      if (day != null)
+      {
+         var dayName = day switch
+         {
+            Day.Mo => "Mo",
+            Day.Mi => "Mi",
+            Day.Fr => "Fr",
+            _ => ""
+         };
+
+         indyHours = indyHours.Where(x => dayName.Equals(x.DayName)).ToList();
+      }
+
+      if (!string.IsNullOrWhiteSpace(teacherId))
+         indyHours = indyHours.Where(x => x.TeacherId.Equals(teacherId)).ToList();
+
+      await MessageHelper.SendListMessageAsync(
+            indyHours,
+            Context,
+            e => $"- {e.TeacherId} ({e.TeacherName}) in {e.Room} on {e.DayName} {e.Hour}\n",
+            "# Indy hours:\n");
+   }
 }
