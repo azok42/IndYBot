3e73950704606d356ef937f0c4be74a751db0973
Azok42 <anton1.hackner@gmail.com>
2026-06-29 11:20:09 +0200

    refactor: move getting client to BeforeExecute function

--- src/Modules/AuthModule.cs
+++ src/Modules/AuthModule.cs
@@ -1,23 +1,39 @@
 using Discord.Interactions;
 using IndYBot.Modules.Services;
+using IndYLib.Interfaces;
 
 namespace IndYBot.Modules;
 
-[Group("get", "Getters")]
+[Group("info", "Getters with needed login")]
 public class AuthModule : InteractionModuleBase<SocketInteractionContext>
 {
    public readonly LoginService _loginService; 
+
+   private IIndyClient? _client = null;
 
    public AuthModule(LoginService loginService)
    {
       _loginService = loginService;
    }
 
+   public override void BeforeExecute(ICommandInfo command)
+   {
+      try
+      {
+         _client = _loginService.getClient(Context.Interaction.User.Id);
+      }
+      catch (KeyNotFoundException)
+      {
+         RespondAsync("## Login needed", ephemeral: true);
+         throw;
+      }
+   }
+
    [SlashCommand("student", "Get info about yourself!")]
    public async Task StudentInfoCommand()
    {
-      var client = (await _loginService.getClient(Context.Interaction.User.Id).GetStudentAsync()).First();
+      var student = (await _client!.GetStudentAsync()).First();
 
-      await RespondAsync($"{client.StudentId}: {client.Firstname} {client.Lastname} {client.Class} ({client.EMail})", ephemeral: true);
+      await RespondAsync($"{student.StudentId}: {student.Firstname} {student.Lastname} {student.Class} ({student.EMail})", ephemeral: true);
    }
 }
