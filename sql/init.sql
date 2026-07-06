630faf43935f6ac37e2ee5c16649660f49e693ae
Azok42 <anton1.hackner@gmail.com>
2026-07-06 19:36:08 +0200

    fix: grant db user needed permissions

--- sql/init.sql
+++ sql/init.sql
@@ -3,6 +3,7 @@ CREATE DATABASE IF NOT EXISTS indy_bot;
 USE indy_bot;
 
 CREATE USER IF NOT EXISTS 'bot'@'localhost' IDENTIFIED BY 'indy_pass';
+GRANT ALL PRIVILEGES ON indy_bot.* TO 'bot'@'localhost';
 
 CREATE TABLE IF NOT EXISTS user (
    id BIGINT PRIMARY KEY,
