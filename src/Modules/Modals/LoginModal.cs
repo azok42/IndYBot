b702ae5104a4556a57096b04dc0982ed2a1f7812
Azok42 <anton1.hackner@gmail.com>
2026-07-07 22:16:50 +0200

    fix: add labels to login modal

--- src/Modules/Modals/LoginModal.cs
+++ src/Modules/Modals/LoginModal.cs
@@ -7,10 +7,12 @@ public class LoginModal : IModal
    public string Title { get; set; } = "Login";
 
    [RequiredInput]
+   [InputLabel("Username", "Your IndY-Username")]
    [ModalTextInput("username", placeholder: "user.name")]
    public string? UsernameInput { get; set; }
 
    [RequiredInput]
+   [InputLabel("Password", "Your IndY-Password")]
    [ModalTextInput("password", placeholder: "pass1234")]
    public string? PasswordInput { get; set; }
 }
