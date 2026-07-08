3780b6dff76199c48142edfad716fd29f9e6dd64
Azok42 <anton1.hackner@gmail.com>
2026-07-08 22:02:33 +0200

    feat: add save command to insert user into database

--- src/Modules/LoginModule.cs
+++ src/Modules/LoginModule.cs
@@ -1,16 +1,20 @@
 using Discord.Interactions;
 using IndYBot.Modules.Modals;
 using IndYBot.Modules.Services;
+using IndYBot.Helpers;
+using Dapper;
 
 namespace IndYBot.Modules;
 
 public class LoginModule : InteractionModuleBase<SocketInteractionContext>
 {
    private readonly LoginService _loginService;
+   private readonly SQLHelper _sqlHelper;
 
-   public LoginModule(LoginService loginService)
+   public LoginModule(LoginService loginService, SQLHelper sqlHelper)
    {
       _loginService = loginService;
+      _sqlHelper = sqlHelper;
    }
 
    [SlashCommand("login", "Login temporarily without saving credentials!")]
@@ -28,5 +32,25 @@ public class LoginModule : InteractionModuleBase<SocketInteractionContext>
       await _loginService.AddClient(Context.Interaction.User.Id, modal.UsernameInput, modal.PasswordInput);
 
       await RespondAsync("Login successful!", ephemeral: true);
+   }
+
+   [SlashCommand("save", "Save username and password for later logins!")]
+   public async Task SaveLoginCommand()
+   {
+      await RespondWithModalAsync<SaveLoginModal>("savelogin-modal");
+   }
+
+   [ModalInteraction("savelogin-modal")]
+   public async Task HandleSaveLoginModal(SaveLoginModal modal)
+   {
+      if (modal.UsernameInput == null)
+         throw new NullReferenceException("Input is null");
+
+      var con = _sqlHelper.CreateConnection();
+
+      var sql = "INSERT INTO user (id, name, password) VALUES (@Id, @Name, @Password);";
+      await con.QueryAsync(sql, new {Id = Context.Interaction.User.Id, Name = modal.UsernameInput, Password = modal.PasswordInput});
+
+      await RespondAsync("Please save successful! Make a '/info student call' to ensure it worked!", ephemeral: true);
    }
 }
---
+++ src/Modules/Modals/SaveLoginModal.cs
@@ -0,0 +1,24 @@
+using Discord.Interactions;
+
+namespace IndYBot.Modules.Modals;
+
+public class SaveLoginModal : IModal
+{
+   public string Title { get; set; } = "Save login data";
+
+   [ModalTextDisplay]
+   public string SaveInfo { get; set; } = "You don't *need* to save your password if you don't want, but you still need to write it at logins!";
+
+   [ModalTextDisplay]
+   public string PasswordInfo { get; set; } = "Your password is going to be saved in **plaintext!!** So not think it's safe!";
+
+   [RequiredInput(true)]
+   [InputLabel("Username", "Your IndY-Username")]
+   [ModalTextInput("username", placeholder: "user.name")]
+   public string? UsernameInput { get; set; }
+
+   [RequiredInput(false)]
+   [InputLabel("Password", "Your IndY-Password")]
+   [ModalTextInput("password", placeholder: "pass1234")]
+   public string? PasswordInput { get; set; } = null;
+}
