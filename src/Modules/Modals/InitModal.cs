478716e714570c4bf329fa19b6ea3fe6e1f816da
Azok42 <anton1.hackner@gmail.com>
2026-08-01 20:16:28 +0200

    fix: make channel select not required in init modal

--- src/Modules/Modals/InitModal.cs
+++ src/Modules/Modals/InitModal.cs
@@ -8,18 +8,22 @@ public class InitModal : IModal
     public string Title { get; } = "Channel setup";
 
     [InputLabel("Default Channel", "Fallback if you don't set other the channels!")]
-    [ModalChannelSelect("default-channel", maxValues: 1)]
+    [ModalChannelSelect("default-channel", minValues: 0, maxValues: 1)]
+    [RequiredInput(false)]
     public ITextChannel[]? DefaultChannel { get; set; }
 
     [InputLabel("Log Channel", "The channel where the logs are being written to!")]
-    [ModalChannelSelect("log-channel", maxValues: 1)]
+    [ModalChannelSelect("log-channel", minValues: 0, maxValues: 1)]
+    [RequiredInput(false)]
     public ITextChannel[]? LogChannel { get; set; }
 
     [InputLabel("Auto-Entry Channel", "The channel where users will be notified about their automatic entries!")]
-    [ModalChannelSelect("auto_entry-channel", maxValues: 1)]
+    [ModalChannelSelect("auto_entry-channel", minValues: 0,  maxValues: 1)]
+    [RequiredInput(false)]
     public ITextChannel[]? AutoEntryChannel { get; set; }
 
     [InputLabel("Group-Entry Channel", "The place where all group-entries are. If not set the entry is written to where the command is from!")]
-    [ModalChannelSelect("group_entry-channel", maxValues: 1)]
+    [ModalChannelSelect("group_entry-channel", minValues: 0, maxValues: 1)]
+    [RequiredInput(false)]
     public ITextChannel[]? GroupEntryChannel { get; set; }
 }
