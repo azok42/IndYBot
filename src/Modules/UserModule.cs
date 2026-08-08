d7e889b654da7c2a29a60bae6acc62c632eac079
Azok42 <anton1.hackner@gmail.com>
2026-08-09 00:56:44 +0200

    feat: implement where is fearture

--- src/Modules/UserModule.cs
+++ src/Modules/UserModule.cs
@@ -3,6 +3,9 @@ using Discord.Interactions;
 using IndYBot.Helpers;
 using IndYBot.Modules.Services;
 using IndYBot.Modules.Preconditions;
+using IndYLib.Interfaces;
+using IndYLib.Services;
+using IndYLib.Models.Entry;
 using Dapper;
 
 namespace IndYBot.Modules;
@@ -29,6 +32,85 @@ public class UserModule : InteractionModuleBase<SocketInteractionContext>
       await user!.ModifyAsync(x => x.Nickname = student.First().Firstname + " " + student.First().Lastname);
 
       await RespondAsync("Successfully changed nickname!");
+   }
+
+   // TODO: optinal day param
+   [SlashCommand("whereis", "Check the entry of the specified user!")]
+   public async Task WhereIsCommand([Summary("user", "The user to be checked")] IUser user)
+   {
+      await DeferAsync(ephemeral: true);
+
+      var con = _sqlHelper.CreateConnection();   
+
+      var sql = "SELECT name, password, whereis_status FROM user WHERE id = @Id;";
+      var queryResult = await con.QueryFirstOrDefaultAsync<(string Username, string Password, string Status)>(sql, new { Id = user.Id });
+
+      if (queryResult == default)
+      {
+         await ModifyOriginalResponseAsync(x => x.Content = "The specified user hasn't saved his credentials and can therefore be not checked!");
+         return;
+      }
+
+      if (!queryResult.Status.Equals("enabled"))
+      {
+         await ModifyOriginalResponseAsync(x => x.Content = "The specified user has the WhereIs-feature disabled. Try remind them to enable it!");
+         return;
+      }
+
+      var today = DateOnly.FromDateTime(DateTime.Today);
+      var indyDays = await IndyClient.GetIndyDaysAsync(today, today.AddDays(7));
+
+      if (!indyDays.Any())
+      {
+         await ModifyOriginalResponseAsync(x => x.Content = "No IndY-Day found! Try it again with setting a date yourself!");
+         return;
+      }
+
+      var indyDayDate = indyDays.First().Date;
+
+      IIndyClient client;
+
+      if (_loginService.HasClient(user.Id))
+         client = _loginService.GetClient(user.Id)!;
+      else
+         client = await _loginService.AddClient(user.Id, queryResult.Username, queryResult.Password);
+
+      var name = user.Username;
+      var entries = await client.GetEntriesAsync(indyDayDate);
+
+      var msg = GetMessageFromEntries(entries, name);
+      
+      await ModifyOriginalResponseAsync(x => x.Content = msg);
+   }
+
+   private string GetMessageFromEntries(FullRetured entries, string name)
+   {
+      var hour3 = entries.Hour3.FirstOrDefault();
+      var hour4 = entries.Hour4.FirstOrDefault();
+
+      if (hour3 == null || hour4 == null)
+         return $"User {name} hasn't made entries!";
+      else
+      {
+         var teacher3 = hour3.TeacherId;
+         var teacher4 = hour4.TeacherId;
+
+         if (hour3 != null && hour4 != null && teacher3.Equals(teacher4))
+            return $"User {name} is at teacher {teacher3}!";
+
+         else if (hour3 != null && hour4 != null && !teacher3.Equals(teacher4))
+            return $"User {name} is at teacher:\n- Hour 3: {teacher3}\n-Hour 4: {teacher4}";
+
+         else if (hour3 != null)
+            return $"User {name} is at teacher:\n- Hour 3: {teacher3}\n-Hour 4: Missing entry";
+
+         else if (hour4 != null)
+            return $"User {name} is at teacher:\n- Hour 3: Missing Entry\n-Hour 4: {teacher4}";
+
+         else
+            return "There was a problem at proccesing hours!";
+      }
+
    }
 
    [SlashCommand("whereis-toogle", "Toogle the WhereIs-feature for your account!")]
