725b705d19ec60203d82509db0d3393dea370422
Azok42 <anton1.hackner@gmail.com>
2026-08-10 22:03:43 +0200

    fix: remove null-ability from save modal and remove save info text

--- src/Modules/Modals/SaveLoginModal.cs
+++ src/Modules/Modals/SaveLoginModal.cs
@@ -7,9 +7,6 @@ public class SaveLoginModal : IModal
    public string Title { get; set; } = "Save login data";
 
    [ModalTextDisplay]
-   public string SaveInfo { get; set; } = "You don't *need* to save your password if you don't want, but you still need to write it at logins!";
-
-   [ModalTextDisplay]
    public string PasswordInfo { get; set; } = "Your password is going to be saved in **plaintext!!** So don't think it's safe!";
 
    [RequiredInput(true)]
@@ -17,8 +14,8 @@ public class SaveLoginModal : IModal
    [ModalTextInput("username", placeholder: "user.name")]
    public string? UsernameInput { get; set; }
 
-   [RequiredInput(false)]
+   [RequiredInput(true)]
    [InputLabel("Password", "Your IndY-Password")]
    [ModalTextInput("password", placeholder: "pass1234")]
-   public string? PasswordInput { get; set; } = null;
+   public string? PasswordInput { get; set; }
 }
